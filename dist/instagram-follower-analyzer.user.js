d2bf5e0d67632094da01cd080c56ed953401d1f7
Cristian Cardenas <ccamilocar@hotmail.com>
2026-02-25 03:28:54 -0500

    feat: bump version to 3.3.1 in user script metadata

--- dist/instagram-follower-analyzer.user.js
+++ dist/instagram-follower-analyzer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Follower Analyzer
 // @namespace    https://github.com/UNKchr/ig-analyzer
-// @version      3.3.0
+// @version      3.3.1
 // @author       UNKchr
 // @description  Analyze Instagram followers and following lists with Anti-Ban retry logic, Progress Bar, CSV Export, and Advanced Metrics.
 // @license      MIT
